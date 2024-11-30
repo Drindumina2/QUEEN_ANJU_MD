905668ebf12faf6ca5485a6c528525d8fcef7a16
Drindumina2 <Drindumina@outlook.com>
2024-11-30 06:09:20 +0530

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=fcUmwDJR#_oTxDSL6lqi8wKvCMLiCuIn1KR94au4D7ZFewBFvUk0",
 };
